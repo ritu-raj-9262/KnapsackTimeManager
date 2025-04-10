b889ad00e4bc7e0f7c1bd91fd7c5f167d0c15095
Ritu Raj <139972749+ritu-raj-9262@users.noreply.github.com>
2025-04-10 13:19:21 +0530

    Update KnapsackSolver.java
    Changing in code knapsack solver

--- KnapsackSolver.java
+++ KnapsackSolver.java
@@ -1,37 +1,55 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class KnapsackSolver {
     public static List<Task> solve(List<Task> tasks, int maxTime) {
-        int n = tasks.size();
-        int[][] dp = new int[n + 1][maxTime + 1];
-        
-        // Build DP table
-        for (int i = 1; i <= n; i++) {
-            Task task = tasks.get(i - 1);
-            for (int w = 1; w <= maxTime; w++) {
-                if (task.time <= w) {
-                    dp[i][w] = Math.max(
-                        dp[i - 1][w],
-                        dp[i - 1][w - task.time] + task.priority
-                    );
-                } else {
-                    dp[i][w] = dp[i - 1][w];
-                }
+        List<FractionalTask> result = new ArrayList<>();
+        List<Task> sortedTasks = new ArrayList<>(tasks);
+
+        // Sort tasks by priority per time unit (descending)
+        Collections.sort(sortedTasks, (a, b) -> Double.compare(
+            (double)b.priority / b.time, (double)a.priority / a.time
+        ));
+
+        int remainingTime = maxTime;
+
+        for (Task task : sortedTasks) {
+            if (task.time <= remainingTime) {
+                result.add(new FractionalTask(task, 1.0)); // full task
+                remainingTime -= task.time;
+            } else {
+                double fraction = (double) remainingTime / task.time;
+                result.add(new FractionalTask(task, fraction)); // partial task
+                break;
             }
         }
-        
-        // Backtrack to find selected tasks
-        List<Task> result = new ArrayList<>();
-        int w = maxTime;
-        for (int i = n; i > 0; i--) {
-            if (dp[i][w] != dp[i - 1][w]) {
-                Task task = tasks.get(i - 1);
-                result.add(task);
-                w -= task.time;
+
+        // Convert FractionalTasks to Task strings with adjusted labels
+        List<Task> finalTasks = new ArrayList<>();
+        for (FractionalTask ft : result) {
+            if (ft.fraction == 1.0) {
+                finalTasks.add(ft.task);
+            } else {
+                finalTasks.add(new Task(
+                    ft.task.name + String.format(" (%.0f%%)", ft.fraction * 100),
+                    (int)(ft.task.time * ft.fraction),
+                    (int)(ft.task.priority * ft.fraction)
+                ));
             }
         }
-        
-        return result;
+
+        return finalTasks;
+    }
+}
+
+// Helper class to represent partially selected tasks
+class FractionalTask {
+    Task task;
+    double fraction;
+
+    public FractionalTask(Task task, double fraction) {
+        this.task = task;
+        this.fraction = fraction;
     }
 }
